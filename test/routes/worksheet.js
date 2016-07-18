f5463b9b20bfdb32c54a35fdcaa2800aebcb20b6
ceeostud2 <ceeostud2@wr-130-64-188-210.medford.tufts.edu>
2016-07-18 14:47:44 -0400

    Added Martin's test

--- test/routes/worksheet.js
+++ test/routes/worksheet.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET Worksheet Pre template listing. */
+router.get('/', function(req, res, next) {
+  res.sendFile(__dirname + '/UItest2.html');
+});
+
 router.post('/', function(req, res, next) {
   console.log(req.body);
   res.send(req.body);
 });
+
 
 module.exports = router;
